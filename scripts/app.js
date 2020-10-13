2ad16ef2fb6c6886b8350179ce6854dd6a345730
trefynwynd <jl.alejandria@gmail.com>
2020-10-12 21:51:32 -0700

    Laid out the variables and comments for the possible functions needed to complete the game.

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+console.log('Insanity Check!')
+// Global variables
+// Array of Objects or Object in an array
+const cardFront
+
+// Array of values
+const cardValue
+
+// function to create the 2x4 arena; I think I should have this already done upon on launch since I was planning for it to be hidden until the "Begin" button is pressed
+// function to flip the cards with the 'click' EventListener (attached to the container)
+// function to randomize what cards are shuffled to where
+// function to compare 2 cards, this should include the part when the cards are flipped back if they are incorrect
+// function for the 8-second timer, which includes what happens if you don't find your m8s in time and when you do find them in time
+// function to start the game
